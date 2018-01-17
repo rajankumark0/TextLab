d6a21aa22de39fb3d121d8793531c01fb45d4997
Rajan <rajankumark0@gmail.com>
2018-01-17 12:06:30 +0530

    dddd

---
+++ www/js/config.js
@@ -0,0 +1,13 @@
+var config =  {
+				    "data":[{
+					    "baseurl":"http://manage.staticking.net/webservice/webservice.php",
+					    "basicurl":"http://manage.staticking.net/",
+					    "app_name" :"DataOxy",
+					    "bundle_id" : "com.dataoxy.com",
+					  "google_ios_clientid":"",
+					  "google_login":"0",
+					 
+                      "tax_text":"0",
+					 
+					  "footer" :""
+			 	    }]}
